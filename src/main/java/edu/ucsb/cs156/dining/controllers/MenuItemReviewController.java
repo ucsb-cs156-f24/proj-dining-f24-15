d4d911a919e63707f9310e3c0d594cd95fe40411
Sophia <sophiaray@ucsb.edu>
2024-11-25 07:56:16 -0800

    updated

---
+++ src/main/java/edu/ucsb/cs156/dining/controllers/MenuItemReviewController.java
@@ -0,0 +1,64 @@
+package edu.ucsb.cs156.dining.controllers;
+
+import edu.ucsb.cs156.dining.entities.MenuItemReview;
+import edu.ucsb.cs156.dining.entities.MenuItem;
+import edu.ucsb.cs156.dining.errors.EntityNotFoundException;
+import edu.ucsb.cs156.dining.repositories.MenuItemReviewRepository;
+import edu.ucsb.cs156.dining.repositories.MenuItemRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+
+import jakarta.validation.Valid;
+import java.time.LocalDateTime;
+
+
+/**
+ * This is a REST controller for MenuItemReview
+ */
+
+@Tag(name = "MenuItemReview")
+@RequestMapping("/api/menuitemreviews")
+@RestController
+@Slf4j
+public class MenuItemReviewController extends ApiController {
+
+    @Autowired
+    MenuItemReviewRepository menuItemReviewRepository;
+
+    @Autowired
+    MenuItemRepository menuItemRepository;
+
+
+    /**
+     * Get all reviews for a given user -> the user that is logged in
+     */
+    @Operation(summary= "Get all reviews for a given user")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/reviews")
+    public Iterable<MenuItemReview> getMenuItemReviewsByUser(Authentication authentication) {
+        String username = authentication.getName();
+        
+        return reviews;
+    }
+
+    
+
+}
--- src/main/java/edu/ucsb/cs156/dining/entities/MenuItem.java
+++ src/main/java/edu/ucsb/cs156/dining/entities/MenuItem.java
@@ -11,7 +11,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 
 /** 
  * This is a JPA entity that represents a MenuItem
