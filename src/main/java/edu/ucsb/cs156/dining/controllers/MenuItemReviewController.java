9e4a6c2c170cf1d7ca9741105459faa8bd9890a0
Sophia <sophiaray@ucsb.edu>
2024-11-26 15:59:29 -0800

    merged

--- src/main/java/edu/ucsb/cs156/dining/controllers/MenuItemReviewController.java
+++ src/main/java/edu/ucsb/cs156/dining/controllers/MenuItemReviewController.java
@@ -2,7 +2,6 @@ package edu.ucsb.cs156.dining.controllers;
 
 import edu.ucsb.cs156.dining.entities.MenuItemReview;
 import edu.ucsb.cs156.dining.entities.MenuItem;
-import edu.ucsb.cs156.dining.errors.EntityNotFoundException;
 import edu.ucsb.cs156.dining.repositories.MenuItemReviewRepository;
 import edu.ucsb.cs156.dining.services.CurrentUserService;
 import edu.ucsb.cs156.dining.repositories.MenuItemRepository;
@@ -16,18 +15,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.http.HttpStatus;
 
-import jakarta.validation.Valid;
 import java.time.LocalDateTime;
 
 
@@ -62,64 +57,9 @@ public class MenuItemReviewController extends ApiController {
         return menuItemReviewRepository.findByStudentUserId(studentUserId);
     }
 
-    
-
-}package edu.ucsb.cs156.dining.controllers;
-
-import edu.ucsb.cs156.dining.entities.MenuItemReview;
-import edu.ucsb.cs156.dining.entities.MenuItem;
-import edu.ucsb.cs156.dining.errors.EntityNotFoundException;
-import edu.ucsb.cs156.dining.repositories.MenuItemReviewRepository;
-import edu.ucsb.cs156.dining.services.CurrentUserService;
-import edu.ucsb.cs156.dining.repositories.MenuItemRepository;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.extern.slf4j.Slf4j;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.http.HttpStatus;
-
-import java.time.LocalDateTime;
-
-
-
-/**
- * This is a REST controller for MenuItemReview
- */
-
-@Tag(name = "MenuItemReview")
-@RequestMapping("/api/menuitemreviews")
-@RestController
-@Slf4j
-public class MenuItemReviewController extends ApiController {
-
-    @Autowired
-    MenuItemReviewRepository menuItemReviewRepository;
-
-    @Autowired
-    MenuItemRepository menuItemRepository;
-
-    @Autowired
-    private CurrentUserService currentUserService;
-    
     /**
      * Create a new menu item review -> all users
      */
-
     @Operation(summary= "Create a new menu item review")
     @PreAuthorize("hasRole('ROLE_USER')")
     @PostMapping("/post")
--- src/test/java/edu/ucsb/cs156/dining/controllers/MenuItemReviewControllerTests.java
+++ src/test/java/edu/ucsb/cs156/dining/controllers/MenuItemReviewControllerTests.java
@@ -175,67 +175,6 @@ public class MenuItemReviewControllerTests extends ControllerTestCase {
                 verify(menuItemRepository, times(1)).findById(1L);
                 verify(menuItemReviewRepository, times(0)).save(any(MenuItemReview.class));
         }
-
-
-}package edu.ucsb.cs156.dining.controllers;
-
-import edu.ucsb.cs156.dining.repositories.UserRepository;
-import edu.ucsb.cs156.dining.services.CurrentUserService;
-import edu.ucsb.cs156.dining.testconfig.TestConfig;
-import edu.ucsb.cs156.dining.ControllerTestCase;
-import edu.ucsb.cs156.dining.entities.MenuItem;
-import edu.ucsb.cs156.dining.entities.MenuItemReview;
-import edu.ucsb.cs156.dining.models.CurrentUser;
-import edu.ucsb.cs156.dining.repositories.MenuItemRepository;
-import edu.ucsb.cs156.dining.repositories.MenuItemReviewRepository;
-import edu.ucsb.cs156.dining.entities.User;
-import edu.ucsb.cs156.dining.models.CurrentUser;
-
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Map;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MvcResult;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-
-import java.time.LocalDateTime;
-
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-@WebMvcTest(controllers = MenuItemReviewController.class)
-@Import(TestConfig.class)
-
-public class MenuItemReviewControllerTests extends ControllerTestCase {
-    @MockBean
-     MenuItemReviewRepository menuItemReviewRepository;
-
-    @MockBean
-    MenuItemRepository menuItemRepository;
-
-    @MockBean
-    UserRepository userRepository;
-
-    @MockBean
-    CurrentUserService currentUserService;
-    
     
     // GET BY ID
     
@@ -264,6 +203,6 @@ public class MenuItemReviewControllerTests extends ControllerTestCase {
         String responseContent = result.getResponse().getContentAsString();
         assertTrue(responseContent.contains("\"id\":1"));
         verify(menuItemReviewRepository, times(1)).findByStudentUserId(100L);
-}
+    }
 
 }
